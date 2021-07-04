76d8f248d97f1913c7d56513f9109d7ceb737fe7
NiverdPereira <niverdpereira@gmail.com>
2021-07-04 14:37:20 +0600

    Update: July 4, 2021 1438Hrs 1. Customized imageView style 2. Added image drawbles

--- app/src/main/java/com/example/amenda/MainActivity.kt
+++ app/src/main/java/com/example/amenda/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.amenda
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBar
+import com.example.amenda.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var binding: ActivityMainBinding? = null
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setActionBar()
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun setActionBar() {
+        val toolBar: Toolbar = findViewById(R.id.toolbar)
+        toolBar.setBackgroundColor(resources.getColor(android.R.color.holo_blue_dark, null))
+        val actionBar = supportActionBar
+        actionBar?.customView = toolBar
+        actionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        actionBar?.setDisplayShowCustomEnabled(true)
     }
 }
